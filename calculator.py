b6798bf6b7039adc140ee2c7a4c29cb386ff85f2
Rajiv Kumar Dubey <158801732+rajivkumardubey01@users.noreply.github.com>
2025-06-28 23:02:49 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,38 @@
+def add(x, y):
+    return x + y
 
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return "Error: Division by zero" if y == 0 else x / y
+
+print("Select operation:\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")
+
+while True:
+    choice = input("Enter choice (1/2/3/4 or q to quit): ")
+
+    if choice == 'q':
+        break
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Try again.")
+            continue
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid choice")
