fc70c7166672fa5f41c40c1c29c4c120396cf2f3
Rajiv Kumar Dubey <158801732+rajivkumardubey01@users.noreply.github.com>
2025-06-28 22:59:44 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,11 @@
+import random, string
 
+try:
+    length = int(input("Password length: "))
+    if length > 0:
+        chars = string.ascii_letters + string.digits + string.punctuation
+        print("Your password:", ''.join(random.choices(chars, k=length)))
+    else:
+        print("Length must be greater than 0.")
+except:
+    print("Enter a valid number.")
